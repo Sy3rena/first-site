58ce95fd873e3dae18b2cc1bb306829f0eb112b7
lilys <104225765+Sy3rena@users.noreply.github.com>
2022-04-22 22:12:39 +0300

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,28 @@
+<footer class="pt-4 my-md-5 pt-md-5 border-top">
+    <div class="row">
+      <div class="col-12 col-md">
+        <small class="d-block mb-3 text-muted">&copy; 2017–2021</small>
+      </div>
+      <div class="col-6 col-md">
+        <h5>О разработчике сайта</h5>
+        <ul class="list-unstyled text-small">
+          <li class="mb-1"><a class="link-secondary text-decoration-none" href="developer.php" target="_blank">Полная статья</a></li>
+          <li class="mb-1"><a class="link-secondary text-decoration-none" href="https://discord.gg/HFMgfqzsxa" target="_blank">Дискорд сервер</a></li>
+          <li class="mb-1"><a class="link-secondary text-decoration-none" href="https://youtu.be/_M8fmoA6WkA" target="_blank">Ютуб</a></li>
+          <li class="mb-1"><a class="link-secondary text-decoration-none" href="https://github.com/Sy3rena/" target="_blank">ГитХаб</a></li>
+          <li class="mb-1"><a class="link-secondary text-decoration-none" href="https://go.mail.ru/search?fr=ps&gp=813091&q=%D1%81%D0%B2%D0%B8%D0%BD%D1%8C%D1%8F" target="_blank">Фотографии хрюши</a></li>
+        </ul>
+      </div>
+      <div class="col-6 col-md">
+        <h5>Ресурсы</h5>
+        <ul class="list-unstyled text-small">
+          <li class="mb-1"><a class="link-secondary text-decoration-none" href="https://monster-book.com/php-7-v-podlinnike" target="_blank">php 7 в подлиннике</a></li>
+          <li class="mb-1"><a class="link-secondary text-decoration-none" href="https://youtu.be/OkaD2dVQqWY" target="_blank">Курс по html</a></li>
+          <li class="mb-1"><a class="link-secondary text-decoration-none" href="https://getbootstrap.com/" target="_blank">Документация бутстрапа</a></li>
+          <li class="mb-1"><a class="link-secondary text-decoration-none" href="https://youtu.be/GfHSbgyHN_I" target="_blank">Курс по php</a></li>
+          <li class="mb-1"><a class="link-secondary text-decoration-none" href="https://github.com/Sy3rena/first-site.git" target="_blank">Исходник сайта</a></li>
+        </ul>
+      </div>
+      </div>
+    </div>
+  </footer>
---
+++ head.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>чоч</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+    </div>
+    <header>
+    <div class="d-flex flex-column flex-md-row align-items-center pb-3 mb-4 border-bottom">
+      <a href="/" class="d-flex align-items-center text-dark text-decoration-none">
+        <span class="fs-4 p-3">SuperMarcet</span>
+      </a>
+
+      <nav class="d-inline-flex mt-2 mt-md-0 ms-md-auto">
+        <a class="me-3 py-2 text-dark text-decoration-none" href="index.php">Главная</a>
+        <a class="me-3 py-2 text-dark text-decoration-none" href="about.php">О нас</a>
+        <a class="me-3 py-2 text-dark text-decoration-none" href="prodject.php">Проект</a>
+      </nav>
+    </div>
+  </header>
+</body>
+</html>
